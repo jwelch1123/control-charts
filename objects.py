d709f44e2f4e755f99b3b8410f795c11d0fc9723
Dissident Genetics <jwelch1123@gmail.com>
2021-12-21 11:50:45 -0500

    Added control chart class and more methods

--- objects.py
+++ objects.py
@@ -26,8 +26,40 @@ class run_chart:
         
         return self
 
+    def from_csv():
+        # parse data from csv into a run_chart object
+        pass
+
+    def from_dataframe():
+        # parse data from dataframe object into a run_chart object
+        pass
+
+    def append(self, new_data, new_labels = None):
+        self.data.append(new_data) 
+        self.labels.append(new_labels)
+        return self
+
+
+    def _check_data(*args):
+        # args is dictionary with "value_name" : value
+        # check that value meets type requirements of value_name
+        pass
+
+
+    
+
     def __str__(self):
-        return "My name's Carl!"
+        return f"\nRun Chart Object \nLabels: {self.labels} \nValues: {self.data} \nRun Average: {self.run_mean}\n"
+
+
+class control_chart(run_chart):
+    
+    Upper_Ctrl_Lmt = int
+    Lower_Ctrl_Lmt = int 
+
+    def __str__(self):
+        return f"\nControl Chart Object \nLabels: {self.labels} \nValues: {self.data} \nRun Average: {self.run_mean}\n"
+        
 
 
 
@@ -35,9 +67,13 @@ a = run_chart()
 a.add_data([0,1,2],["1st","2nd","3rd"])
 print(a)
 
-b = run_chart()
-b.add_data([0,"1",2],["1st","2nd","3rd"])
-print(b)
+#b = run_chart()
+#b.add_data([0,"1",2],["1st","2nd","3rd"])
+#print(b)
+
+c = control_chart()
+c.add_data([0,1,2],["1st","2nd","3rd"])
+print(c)
 
 
 
