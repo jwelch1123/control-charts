0f8fc1cf2dd844896fe9af1ffb9bc0d999b244ba
Dissident Genetics <jwelch1123@gmail.com>
2021-12-23 10:25:34 -0500

    Updated append, and label validation

--- objects.py
+++ objects.py
@@ -1,50 +1,63 @@
 
 class run_chart:
 
-    data     = []
-    labels   = []
+    data     = list
+    labels   = list
     run_mean = int
+    Grouping       = list
     
-    def add_data(self, run_data, labels = None):
+    def _check_labels(self, labels,data_length, append = False):
+        if isinstance(labels, type(None)):
+            labels = list(range(1,data_length))
+            if append:
+                labels = [x+ data_length for x in labels]
+        else:
+            if data_length != len(labels):
+                raise ValueError("Labels should be the same length as run_data.")
+        return labels
 
+
+    def add_data(self, run_data, labels = None):
         # Check data can be coerced to numeric
         try:
             sum(run_data)
         except:
-            raise TypeError("Run data contains non-numeric types.")
+            raise TypeError("Data contains non-numeric types.")
             
-        # Assign labels if missing, check length is equivalent to run_data
-        if isinstance(labels, type(None)):
-            labels = range(1,len(run_data))
-        else:
-            if len(run_data) != len(labels):
-                raise ValueError("Labels should be the same length as run_data.")
-
         self.data = run_data
-        self.labels = labels
+        self.labels = self._check_labels(labels, len(run_data))
         self.run_mean = sum(run_data)/len(run_data)
         
         return self
 
-    def from_csv():
+    def from_csv(self, data_col, label_col, grouping_col):
         # parse data from csv into a run_chart object
         pass
 
-    def from_dataframe():
+    def from_dataframe(self, data_col, label_col, grouping_col):
         # parse data from dataframe object into a run_chart object
         pass
 
+    def to_csv():
+        pass
+
+    def to_dataframe():
+        pass
+
     def append(self, new_data, new_labels = None):
-        self.data.append(new_data) 
-        self.labels.append(new_labels)
+        self.data.extend(new_data) 
+        self.labels.extend(self._check_labels(new_labels, len(new_data), append=True))
+        self.run_mean = sum(self.data)/len(self.data)
         return self
 
 
     def _check_data(*args):
         # args is dictionary with "value_name" : value
         # check that value meets type requirements of value_name
+        validation = {"Data": sum}
         pass
 
+    
 
     
 
@@ -56,6 +69,8 @@ class control_chart(run_chart):
     
     Upper_Ctrl_Lmt = int
     Lower_Ctrl_Lmt = int 
+
+    
 
     def __str__(self):
         return f"\nControl Chart Object \nLabels: {self.labels} \nValues: {self.data} \nRun Average: {self.run_mean}\n"
@@ -73,6 +88,7 @@ print(a)
 
 c = control_chart()
 c.add_data([0,1,2],["1st","2nd","3rd"])
+c.append([5,8,20])
 print(c)
 
 
