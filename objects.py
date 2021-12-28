b436f222da0db75a6765d1a7c57221291f9d2761
Dissident Genetics <jwelch1123@gmail.com>
2021-12-28 15:32:44 -0500

    Simplified data addition

--- objects.py
+++ objects.py
@@ -1,33 +1,43 @@
 
 class run_chart:
 
-    data     = list
-    labels   = list
+    def __init__(self) -> None:
+        pass
+
+    data     = []
+    labels   = []
     run_mean = int
-    Grouping       = list
+    grouping = list
     
-    def _check_labels(self, labels,data_length, append = False):
-        if isinstance(labels, type(None)):
-            labels = list(range(1,data_length))
-            if append:
-                labels = [x+ data_length for x in labels]
+    def _check_labels(self, new_labels, data_length):
+        # uploading new data -> labels empty
+        # appending data (no provided labels) -> numbers with increment
+        # appending data (provided labels) -> return provided labels if correct length
+
+        if isinstance(new_labels, type(None)):
+            new_labels = list(range(len(self.labels), len(self.labels + data_length)))
         else:
-            if data_length != len(labels):
-                raise ValueError("Labels should be the same length as run_data.")
-        return labels
+            if data_length != len(new_labels):
+                raise ValueError("Provided labels should be the same length as provided data.")
+        return new_labels
+
+    def _update_attributes(self):
+        self.run_mean = sum(self.data)/len(self.data)
+
+        return self
 
 
-    def add_data(self, run_data, labels = None):
+    def add_data(self, new_data, new_labels = None):
         # Check data can be coerced to numeric
         try:
-            sum(run_data)
+            sum(new_data)
         except:
             raise TypeError("Data contains non-numeric types.")
-            
-        self.data = run_data
-        self.labels = self._check_labels(labels, len(run_data))
-        self.run_mean = sum(run_data)/len(run_data)
-        
+
+        #Append new data and labels to existing data.    
+        self.data.extend(new_data) 
+        self.labels.extend(self._check_labels(new_labels, len(new_data)))
+        self._update_attributes()
         return self
 
     def from_csv(self, data_col, label_col, grouping_col):
@@ -39,16 +49,12 @@ class run_chart:
         pass
 
     def to_csv():
+        # converts csv to dataframe and parses object. 
+        # not suggested unless you know what the columns contain
         pass
 
     def to_dataframe():
         pass
-
-    def append(self, new_data, new_labels = None):
-        self.data.extend(new_data) 
-        self.labels.extend(self._check_labels(new_labels, len(new_data), append=True))
-        self.run_mean = sum(self.data)/len(self.data)
-        return self
 
 
     def _check_data(*args):
@@ -70,26 +76,36 @@ class control_chart(run_chart):
     Upper_Ctrl_Lmt = int
     Lower_Ctrl_Lmt = int 
 
-    
+    def _update_attributes(self):
+        self.run_mean = sum(self.data)/len(self.data)
+        
+        # make modifiable
+        #self.Upper_Ctrl_Lmt = 
+        #self.Lower_Ctrl_Lmt = 
+        return self
 
     def __str__(self):
         return f"\nControl Chart Object \nLabels: {self.labels} \nValues: {self.data} \nRun Average: {self.run_mean}\n"
         
 
 
-
+print("Regular addition with update")
 a = run_chart()
 a.add_data([0,1,2],["1st","2nd","3rd"])
 print(a)
+a.add_data([3,4,5], ["4th","5th","6th"])
+print(a)
 
-#b = run_chart()
-#b.add_data([0,"1",2],["1st","2nd","3rd"])
-#print(b)
+print("\n")
+print("Fail data type")
+b = run_chart()
+b.add_data([0,"1",2],["1st","2nd","3rd"])
+print(b)
 
-c = control_chart()
-c.add_data([0,1,2],["1st","2nd","3rd"])
-c.append([5,8,20])
-print(c)
+#c = control_chart()
+#c.add_data([0,1,2],["1st","2nd","3rd"])
+#c.append([5,8,20])
+#print(c)
 
 
 
